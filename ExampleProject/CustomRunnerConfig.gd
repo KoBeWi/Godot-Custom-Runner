3d38d2e61bc1c645025c2415c7b271680083c806
kobewi <kobewi4e@gmail.com>
2025-12-29 23:30:36 +0100

    Rework example project

--- ExampleProject/CustomRunnerConfig.gd
+++ ExampleProject/CustomRunnerConfig.gd
@@ -3,5 +3,8 @@ extends CustomRunner
 func _can_play_scene(scene: Node) -> bool:
 	return scene is Level
 
+func _gather_variables(scene: Node):
+	add_variable("mouse_pos", scene.get_local_mouse_position())
+
 func _get_game_scene(for_scene: Node) -> String:
-	return "res://Game.tscn"
+	return "uid://bpv0gxik3m0dj"
--- ExampleProject/addons/CustomRunner/Plugin.gd
+++
@@ -1,7 +0,0 @@
-@tool
-extends EditorPlugin
-
-func _enter_tree():
-	var runner := preload("Config.gd").new()
-	runner.plugin = self
-	add_child(runner)
