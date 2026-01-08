71169a52601265febc5cbceff5bbf52d255d5b12
kobewi <kobewi4e@gmail.com>
2026-01-08 17:01:53 +0100

    Upgrade project

--- ExampleProject/CustomRunnerConfig.gd
+++ ExampleProject/CustomRunnerConfig.gd
@@ -4,7 +4,7 @@ func _can_play_scene(scene: Node) -> bool:
 	return scene is Level
 
 func _gather_variables(scene: Node):
-	add_variable("mouse_pos", scene.get_local_mouse_position())
+	add_variable("mouse_pos", get_click_position())
 
 func _get_game_scene(for_scene: Node) -> String:
 	return "uid://bpv0gxik3m0dj"
